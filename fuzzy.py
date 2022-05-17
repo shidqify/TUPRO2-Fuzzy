5e4f252a8babbe3d39e305bd43f6c17697434166
Shidqi Yusri <shidqifadhlu@gmail.com>
2022-05-17 22:13:28 +0700

    define some function

--- fuzzy.py
+++ fuzzy.py
@@ -1,2 +1,50 @@
+import pandas as pd
+from operator import itemgetter
+from numpy import array
+from copy import deepcopy
+
+'''
+    Price :
+        - Cheap
+        - Moderate
+        - Expensive
+
+    Quality :
+        - Worst
+        - Bad
+        - Average
+        - Good
+        - Best
+'''
+
+def read_data(filename):
+    pass
+
+# Price
+def cheap_price(price):
+    pass
+
+def moderate_price(price):
+    pass
+
+def expensive_price(price):
+    pass
+
+# Quality
+def worst_quality(quality):
+    pass
+
+def bad_quality(quality):
+    pass
+
+def average_quality(quality):
+    pass
+
+def good_quality(quality):
+    pass
+
+def best_quality(quality):
+    pass
+
 if __name__=="__main__":
     print("Hello World!")
